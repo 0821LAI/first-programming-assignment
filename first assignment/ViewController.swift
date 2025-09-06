67881257abbd8c0821d72acb4543a42819f57f2e
Lai <tanglai0821@gmail.com>
2025-09-05 23:44:46 -0400

    Initial Commit

---
+++ first assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first assignment
+//
+//  Created by lai Tang on 9/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ first assignmentTests/first_assignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  first_assignmentTests.swift
+//  first assignmentTests
+//
+//  Created by lai Tang on 9/5/25.
+//
+
+import Testing
+@testable import first_assignment
+
+struct first_assignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
