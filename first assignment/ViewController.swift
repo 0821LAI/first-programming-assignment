e2850155ef848bd65a7a897cbf97b299d4e73e7c
0821LAI <tanglai0821@gmail.com>
2025-09-07 01:51:41 -0400

    Set up limitation in the food query & backgound color change

--- first assignment/ViewController.swift
+++ first assignment/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .white
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
@@ -29,7 +29,8 @@ class ViewController: UIViewController {
     func getRandomPhoto() {
         let baseURL = "https://api.unsplash.com/photos/random"
         let accessKey = "LqGBgAw6UCbhjpxEC2v5Fto7cqx29XHR8VELKunT3kw"
-        let query = "food"
+        let foods = ["pizza", "burger", "sushi", "pasta", "tacos", "ramen", "curry"]
+        let query = foods.randomElement() ?? "food"
         let urlString = "\(baseURL)?client_id=\(accessKey)&query=\(query)"
         guard let url = URL(string: urlString) else {
             print("Invalid URL")
